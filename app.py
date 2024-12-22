b5649e1902220c71059712f5654997fcc682dab0
Kelniit <kelpmul01@gmail.com>
2024-12-23 00:35:19 +0700

    Hi ! Apriori !

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+from HelperRules import HelperGetRules
+
+import pandas
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def MainRoute():
+  """
+  Main Route
+  """
+  return jsonify({"message":"Year Application !"})
+
+@app.route("/GetRules", methods=["POST"])
+def GetFinalRules():
+  """
+  Set Min. Support & Set Min. Confi
+
+  >>> {"support" : 0.05, "confidence" : 0.3}
+  """
+  params = request.json
+  support = float(params.get("support", 0.05))
+  confi = float(params.get("confidence", 0.3))
+  result = HelperGetRules(support, confi)
+  return jsonify(result)
+
+if __name__ == '__main__':
+  # Application Runner
+  port = "5000"
+  host = "127.0.0.1"
+  app.run(host=host, port=port)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Year
+
+Year : Market Basket Analysis Using Apriori !
+
+## 💻 Year Technologies
+
+[![Our Tech](https://skillicons.dev/icons?i=python,flask)](https://skillicons.dev)
